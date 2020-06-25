6f35cf4276570c2266448aa1c832e27bc08faa9c
Fernando Alamillo <fernando.alamillo@creative-technologies.co.uk>
2020-06-25 17:06:55 +0100

    feat(jest) setting up tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import '@testing-library/jest-dom/extend-expect';
+import Enzyme from 'enzyme';
+import ReactSixteenAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new ReactSixteenAdapter() });
